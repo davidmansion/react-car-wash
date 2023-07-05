71863f6c55f218469381b367cfc155cfb72f61aa
Tar Bence <tarbence1@gmail.com>
2023-07-05 20:08:54 +0200

    Fix useEffect

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from "react";
 
 export const Weather = () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=47.793&lon=18.960&appid=${process.env.WEATHER_API_KEY}`
     const [temperature, setTemperature] = useState<number | null>(null)
 
-    const handleFetchData = async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        if (typeof data.main !== "undefined") {
-            const celsius = parseInt(data.main.temp) / 10
-            setTemperature(celsius)
-        }
-
-    }
-
     useEffect(() => {
-        handleFetchData();
-    }, [handleFetchData])
+        const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=47.793&lon=18.960&appid=${process.env.WEATHER_API_KEY}`
+        const fetchData = async () => {
+          try {
+            const response = await fetch(api_url);
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            if (typeof data.main !== 'undefined') {
+              const celsius = parseFloat(data.main.temp);
+              setTemperature(celsius);
+            }
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+      
+        fetchData();
+      });
 
     return (
         <div className="absolute top-16 right-2 lg:right-8 z-40 font-bold text-xl md:text-2xl lg:text-4xl text-white">
