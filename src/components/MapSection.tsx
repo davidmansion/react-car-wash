d7c0409b7d0d77583cc1098ad7835a2229441c08
davidmansion <93778907+davidmansion@users.noreply.github.com>
2024-02-05 09:09:41 +0000

    Update MapSection.tsx

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -3,7 +3,7 @@ export const MapSection = () => (
         <div className="relative w-full h-[400px] lg:px-16 px-4" data-aos="fade-up">
             <div className="w-full h-[400px]">
                 <iframe className="rounded-2xl" width="100%" height={400} title="Google maps"
-                    src="<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1160.204030268583!2d-1.9571047435951103!3d51.96336394454486!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1707121480804!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>">
+                    src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1160.204030268583!2d-1.9571047435951103!3d51.96336394454486!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1707121480804!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
                 </iframe>
             </div>
         </div>
